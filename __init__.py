3b184f7b680020de7ec36dc3f525b1e786db397e
shrn82256 <shrn82256@gmail.com>
2018-10-21 19:18:27 +0530

    some changes

--- __init__.py
+++ __init__.py
@@ -1,10 +1,25 @@
 import models
-from data import AlphaVantage
+from mpi4py import MPI
+from data import AlphaVantage, Stock
+from pprint import pprint
+import pandas as pd
+
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+
+NASDAQ_LIST_FILE = "db/companylist.csv"
+PER_NODE_LIMIT = 100
 
 
 def main():
-    stock = AlphaVantage.AlphaVantage()
-    print(stock.get_data())
+    if rank == 0:
+        # stock = AlphaVantage.AlphaVantage()
+        # stock_data = stock.get_data()
+        # print(len([i for i in stock_data['Time Series (1min)'] if i.startswith('2018-10-18')]))
+        stocks = pd.read_csv(NASDAQ_LIST_FILE)
+
+        print(stocks.summary_quote.head())
 
 
 if __name__ == "__main__":
--- data/AlphaVantage.py
+++ data/AlphaVantage.py
@@ -4,15 +4,15 @@ import requests
 class AlphaVantage:
 
     def __init__(self):
-        # data = self.data
-
         self.function_name = "TIME_SERIES_INTRADAY"
         self.symbol = "MSFT"
         self.interval = "1min"
         self.apikey = "GUPPO7FAKF3SENRJ"
         self.apiurl = "https://www.alphavantage.co/query?"
+        self.outputsize = "full"
 
     def get_data(self):
-        url = "https://www.alphavantage.co/query?function=%s&symbol=%s&interval=%s&apikey=%s" \
-              % (self.function_name, self.symbol, self.interval, self.apikey)
-        return url
+        url = "https://www.alphavantage.co/query?function=%s&symbol=%s&interval=%s&apikey=%s&outputsize=%s" \
+              % (self.function_name, self.symbol, self.interval, self.apikey, self.outputsize)
+
+        return requests.get(url).json()
